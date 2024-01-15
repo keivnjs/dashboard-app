bc92cc2c65f19f7d2388c634251f31fb162c59cf
keivnjs <kjsurtan@gmail.com>
2024-01-15 16:06:50 +0700

    feat: add blogTable for dashboard

--- app/dashboard/components/BlogTable.tsx
+++ app/dashboard/components/BlogTable.tsx
@@ -1,9 +1,16 @@
 import { Button } from "@/components/ui/button";
+import { Switch } from "@/components/ui/switch";
+import { readBlogAdmin, updateBlogById } from "@/lib/actions/blog";
 import { EyeOpenIcon, Pencil1Icon, TrashIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
 import React from "react";
+import DeleteAlert from "./DeleteAlert";
+import SwitchForm from "./SwitchForm";
+import { BlogFormSchemaType } from "../schema";
 
-export default function BlogTable() {
+export default async function BlogTable() {
+  const { data: blogs } = await readBlogAdmin();
+
   return (
     <>
       <div className="rounded-md border-[0.5px] overflow-y-scroll ">
@@ -12,18 +19,31 @@ export default function BlogTable() {
             <h1 className=" col-span-2">Title</h1>
             <h1>Publish</h1>
           </div>
-          <div className="grid grid-cols-5 border-b p-5">
-            <h1 className=" col-span-2">Title</h1>
-            <h1>Title</h1>
-            <Actions />
-          </div>
+
+          {blogs?.map((blog, index) => {
+            const updatePulished = updateBlogById.bind(null, blog.id, {
+              is_published: !blog.is_published,
+            } as BlogFormSchemaType);
+
+            return (
+              <div className="grid grid-cols-5 p-5" key={index}>
+                <h1 className="col-span-2">{blog.title}</h1>
+                <SwitchForm
+                  checked={blog.is_published}
+                  name="publish"
+                  onToggle={updatePulished}
+                />
+                <Actions id={blog.id} />
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
   );
 }
 
-const Actions = () => {
+const Actions = ({ id }: { id: string }) => {
   return (
     <div className="flex items-center gap-2">
       <Link href={`/blog/`}>
@@ -33,14 +53,9 @@ const Actions = () => {
         </Button>
       </Link>
 
-      <Link href={`/blog/`}>
-        <Button className="flex gap-2 items-center" variant="outline">
-          <TrashIcon />
-          Delete
-        </Button>
-      </Link>
+      <DeleteAlert blogId={id} />
 
-      <Link href={`/dashboard/blog/edit/`}>
+      <Link href={"/dashboard/blog/edit/" + id}>
         <Button className="flex gap-2 items-center" variant="outline">
           <Pencil1Icon />
           Edit
