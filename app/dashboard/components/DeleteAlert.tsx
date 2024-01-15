2463255eb1c09fea8db66cdb9b282caa7392ae42
keivnjs <kjsurtan@gmail.com>
2024-01-15 16:07:23 +0700

    feat: delete function for dashboard

---
+++ app/dashboard/components/DeleteAlert.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { TrashIcon } from "@radix-ui/react-icons";
+import React, { ChangeEvent, useTransition } from "react";
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { deleteBlogById } from "@/lib/actions/blog";
+import { toast } from "@/components/ui/use-toast";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import { cn } from "@/lib/utils";
+
+export default function DeleteAlert({ blogId }: { blogId: string }) {
+  const [isPending, startTransition] = useTransition();
+  const onSubmit = async (e: ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    startTransition(async () => {
+      const result = await deleteBlogById(blogId);
+      const { error } = JSON.parse(result);
+      if (error) {
+        toast({
+          title: "Fail to update ",
+          description: (
+            <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+              <code className="text-white">{error?.message}</code>
+            </pre>
+          ),
+        });
+      } else {
+        toast({
+          title: "Successfully delete 🎉",
+        });
+      }
+    });
+  };
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button className="flex gap-2 items-center" variant="outline">
+          <TrashIcon />
+          Delete
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your blog
+            and remove your data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <form onSubmit={onSubmit}>
+            <Button className="flex gap-2 items-center">
+              <AiOutlineLoading3Quarters
+                className={cn(" animate-spin ", {
+                  hidden: !isPending,
+                })}
+              />{" "}
+              Continue
+            </Button>
+          </form>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
