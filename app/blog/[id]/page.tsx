7af1731f5aff7a79d07d9d614374d26ed6b60c25
keivnjs <kjsurtan@gmail.com>
2024-01-16 14:06:40 +0700

    fix: change api route

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,23 +1,35 @@
 import Image from "next/image";
 import { IBlog } from "@/lib/types";
 import Content from "./components/Content";
-import { SITE_URL } from "@/shared/utils/constants";
+import {
+  NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  NEXT_PUBLIC_SUPABASE_URL,
+  SITE_URL,
+} from "@/shared/utils/constants";
+import { headers } from "next/headers";
+import { Database } from "@/lib/types/supabase";
+import { createClient } from "@supabase/supabase-js";
 // import Content from "./components/Content";
 
-export async function generateStaticParams() {
-  const { data: blogs } = await fetch(SITE_URL + "/api/blog?id=*").then((res) =>
-    res.json()
-  );
+const supabase = createClient<Database>(
+  NEXT_PUBLIC_SUPABASE_URL!,
+  NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
 
-  return blogs;
+export async function generateStaticParams() {
+  const result = await supabase.from("blog").select("id").limit(10);
+
+  return result.data;
 }
 
 export default async function page({ params }: { params: { id: string } }) {
-  const { data: blog } = (await fetch(
-    SITE_URL + "/api/blog?id=" + params.id
-  ).then((res) => res.json())) as { data: IBlog };
+  const result = await supabase
+    .from("blog")
+    .select("*")
+    .eq("id", params.id)
+    .single();
 
-  if (!blog?.id) {
+  if (!result.data?.id) {
     return <h1 className="text-white">Not found</h1>;
   }
 
@@ -25,17 +37,17 @@ export default async function page({ params }: { params: { id: string } }) {
     <div className="max-w-5xl mx-auto min-h-screen  pt-10 space-y-10">
       <div className="sm:px-10 space-y-5">
         <h1 className=" text-3xl font-bold dark:text-gray-200">
-          {blog?.title}
+          {result.data?.title}
         </h1>
         <p className="text-sm dark:text-gray-400">
-          {new Date(blog?.created_at!).toDateString()}
+          {new Date(result.data?.created_at!).toDateString()}
         </p>
       </div>
 
       <div className="w-full h-96 relative">
         <Image
           priority
-          src={blog?.image_url!}
+          src={result.data?.image_url!}
           alt="cover"
           fill
           className=" object-cover object-center rounded-md border-[0.5px] border-zinc-600"
